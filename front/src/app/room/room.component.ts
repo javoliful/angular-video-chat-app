f7da6adb87412b961c344618272faa6ad4fec253
javoliful <javier.oliverfulguera@aareon.com>
2021-05-24 18:48:34 +0200

    * change order emmit

--- front/src/app/room/room.component.ts
+++ front/src/app/room/room.component.ts
@@ -44,68 +44,81 @@ export class RoomComponent implements OnInit {
     const myPeer = new Peer(this.currentUserId, { url: 'https://9000-magenta-dinosaur-xc650j6e.ws-eu07.gitpod.io' });
     const config: SocketIoConfig = { url: '/', options: {} };
     this.socket = new Socket(config)
-    
+
     this.route.params.subscribe((params) => {
       console.log(params);
       console.log(`Suscription to Peer.open with id ${this.currentUserId}`);
       myPeer.on('open', userId => {
-        console.log('Received open event (roomId, userId)', params.roomId, userId);
-        this.socket.emit('join-room', params.roomId, userId);
+        console.log('Received open event - Peer (roomId, userId)', params.roomId, userId);
+        
+        /* get media */
+         navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      })
+        .catch((err) => {
+          console.error('[Error] Not able to retrieve user media:', err);
+          return null;
+        })
+        .then((myStream: MediaStream | null) => {
+          if (myStream) {
+            this.addMyVideo(myStream);
+          }
+          console.log('Suscribeing to call...(myStream)', myStream);
+          myPeer.on('call', (call) => {
+            console.log('receiving call...', call);
+            call.answer(myStream);
+
+            call.on('stream', (otherUserVideoStream: MediaStream) => {
+              console.log('receiving other stream', otherUserVideoStream);
+
+              this.addOtherUserVideo(call.metadata.userId, otherUserVideoStream);
+            });
+
+            call.on('error', (err) => {
+              console.error(err);
+            })
+          });
+
+          this.socket.on('user-connected', (otherUserId) => {
+            console.log('1/5 - Receiving user-connected event', `Calling ( otherUserId ) ${otherUserId}`);
+            //for ${roomName} witch name ${userName}
+
+            // Let some time for new peers to be able to answer
+            setTimeout(() => {
+              console.log('2/5 -  peer.call (otherUserId, myStream) ', otherUserId, myStream);
+              const call = myPeer.call(otherUserId, myStream, {
+                metadata: { userId: this.currentUserId },
+              });
+              myPeer.on('error', function (err) { console.log(err, 'Error'); });
+              console.log('Suscribe *stream* other user: ', otherUserId);
+              call.on('stream', (otherUserVideoStream: MediaStream) => {
+                console.log('3/5 Receiving other user stream after his connection (otherUserId, otherUserVideoStream)', otherUserId, otherUserVideoStream);
+                this.addOtherUserVideo(otherUserId, otherUserVideoStream);
+              });
+              console.log('Suscribe *close* other user (otherUserId): ', otherUserId);
+              call.on('close', () => {
+                console.log('Received *close* event (otherUserId): ', otherUserId);
+                this.videos = this.videos.filter((video) => video.userId !== otherUserId);
+              });
+            }, 1000);
+          });
+
+           this.socket.emit('join-room', params.roomId, userId);
+           console.log('Emmited join-room event (roomId, userId)', params.roomId, userId);
+
+        });
+
+
+
+         
+        
+       
       });
     });
     
-    navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    })
-      .catch((err) => {
-        console.error('[Error] Not able to retrieve user media:', err);
-        return null;
-      })
-      .then((myStream: MediaStream | null) => {
-        if (myStream) {
-          this.addMyVideo(myStream);
-        }
-        console.log('Suscribeing to call...(myStream)', myStream);
-        myPeer.on('call', (call) => {
-          console.log('receiving call...', call);
-          call.answer(myStream);
-
-          call.on('stream', (otherUserVideoStream: MediaStream) => {
-            console.log('receiving other stream', otherUserVideoStream);
-
-            this.addOtherUserVideo(call.metadata.userId, otherUserVideoStream);
-          });
-
-          call.on('error', (err) => {
-            console.error(err);
-          })
-        });
-
-        this.socket.on('user-connected', (otherUserId) => {
-          console.log('1/5 - Receiving user-connected event', `Calling ( otherUserId ) ${otherUserId}`);
-          //for ${roomName} witch name ${userName}
-
-          // Let some time for new peers to be able to answer
-          setTimeout(() => {
-            console.log('2/5 -  peer.call (otherUserId, myStream) ', otherUserId, myStream);
-            const call = myPeer.call(otherUserId, myStream, {
-              metadata: { userId: this.currentUserId },
-            });
-            console.log('Suscribe *stream* other user: ',otherUserId );
-            call.on('stream', (otherUserVideoStream: MediaStream) => {
-              console.log('3/5 Receiving other user stream after his connection (otherUserId, otherUserVideoStream)',otherUserId, otherUserVideoStream);
-              this.addOtherUserVideo(otherUserId, otherUserVideoStream);
-            });
-            console.log('Suscribe *close* other user (otherUserId): ',otherUserId );
-            call.on('close', () => {
-              console.log('Received *close* event (otherUserId): ',otherUserId );
-              this.videos = this.videos.filter((video) => video.userId !== otherUserId);
-            });
-          }, 1000);
-        });
-      });
-
+     
+    
     this.socket.on('user-disconnected', (myUserId) => {
       console.log(`receiving user-disconnected event from (myUserId): ${myUserId}`)
       this.videos = this.videos.filter(video => video.userId !== myUserId);
